93b35537845d7bcb02db6fb19f38599fc274b490
Hardik Bansal <bansalhardik31@gmail.com>
2026-04-16 17:56:58 +0530

    crud works

--- College-ManageMent-System/src/app/view/services/hostel/hostel.service.ts
+++ College-ManageMent-System/src/app/view/services/hostel/hostel.service.ts
@@ -20,31 +20,31 @@ export class HostelService {
 
   // CREATE
   addHostel(data: HostelReq): Observable<any> {
-    return this.http.post("/api/hostels", data, { headers: this.getHeaders() });
+    return this.http.post("/api/hostel", data, { headers: this.getHeaders() });
   }
 
   // READ ALL
   getHostels(): Observable<HostelRes[]> {
-    return this.http.get<HostelRes[]>("/api/hostels", { headers: this.getHeaders() });
+    return this.http.get<HostelRes[]>("/api/hostel", { headers: this.getHeaders() });
   }
 
   // READ ONE
   getHostel(id: number | string): Observable<HostelRes> {
-    return this.http.get<HostelRes>(`/api/hostels/${id}`, { headers: this.getHeaders() });
+    return this.http.get<HostelRes>(`/api/hostel/${id}`, { headers: this.getHeaders() });
   }
 
   // UPDATE (PUT)
   updateHostel(id: number | string, data: HostelReq): Observable<HostelRes> {
-    return this.http.put<HostelRes>(`/api/hostels/${id}`, data, { headers: this.getHeaders() });
+    return this.http.put<HostelRes>(`/api/hostel/${id}`, data, { headers: this.getHeaders() });
   }
 
   // PATCH
   patchHostel(id: number | string, data: Partial<HostelReq>): Observable<HostelRes> {
-    return this.http.patch<HostelRes>(`/api/hostels/${id}`, data, { headers: this.getHeaders() });
+    return this.http.patch<HostelRes>(`/api/hostel/${id}`, data, { headers: this.getHeaders() });
   }
 
   // DELETE
   deleteHostel(id: number | string): Observable<any> {
-    return this.http.delete(`/api/hostels/${id}`, { headers: this.getHeaders() });
+    return this.http.delete(`/api/hostel/${id}`, { headers: this.getHeaders() });
   }
 }
