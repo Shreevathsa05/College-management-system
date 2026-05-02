40e775a994ace24873f67a78b7ae905e1d0c6f99
Abhinav Dwivedi <photon278@gmail.com>
2026-05-02 15:17:01 +0530

    Added search functionality

--- FSD_PROJECT-main/src/main/java/com/ayush/College_Management_System/controller/StudentController.java
+++ FSD_PROJECT-main/src/main/java/com/ayush/College_Management_System/controller/StudentController.java
@@ -12,7 +12,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 import com.ayush.College_Management_System.model.enums.AdmissionType;
