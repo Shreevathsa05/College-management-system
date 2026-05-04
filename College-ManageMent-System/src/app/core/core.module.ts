a6c4622863569227fd3ca49e91a96098ad2d6249
Abhinav Dwivedi <photon278@gmail.com>
2026-05-04 22:41:53 +0530

    Make changes

--- College-ManageMent-System/src/app/core/core.module.ts
+++ College-ManageMent-System/src/app/core/core.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './components/layout/header/header.component';
 import { FooterComponent } from './components/layout/footer/footer.component';
 import { LandingComponent } from './components/layout/landing/landing.component';
